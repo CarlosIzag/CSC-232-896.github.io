6cd980fd2af5784a4ab0b0577f74e33401f4c505
CarlosIzag <67172169+CarlosIzag@users.noreply.github.com>
2020-06-26 11:17:10 -0500

    Update Rational.h

--- HOMEWORK/Hw2/Rational.h
+++ HOMEWORK/Hw2/Rational.h
@@ -12,13 +12,18 @@ class Rational
 {
     //
     private:
-
+        int numer;
+        int denom;
+        static int gcd(int, int)
 
 
 
     //
     public:
         //Overloaded Constructor
+        Rational();
+        Rational(int n = 0, int d = 1);
+        void simplify();
 
 
 };
